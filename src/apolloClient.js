6d2e71568da51a61f34072756b6b7ee12e9533e0
yoojat <yooja.ty@gmail.com>
2018-08-01 09:44:05 +0900

    Getting data from the GraphQL API

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  url: "https://movieql-osezlvyqsg.now.sh/"
+  uri: "https://movieql-osezlvyqsg.now.sh"
 });
 
 export default client;
